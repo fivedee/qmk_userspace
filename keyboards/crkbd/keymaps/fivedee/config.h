287b98effd6b3345597515ce5c05066ca4d0ca74
fivedee <64674962+fivedee@users.noreply.github.com>
2025-08-13 11:03:21 -0700

    Add EE_HANDS

--- keyboards/crkbd/keymaps/fivedee/config.h
+++ keyboards/crkbd/keymaps/fivedee/config.h
@@ -24,6 +24,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #define FORCE_NKRO
 
+#define EE_HANDS
+
 #define TAPPING_TERM 175
 #define CHORDAL_HOLD
 #define FLOW_TAP_TERM 125
