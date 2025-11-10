a43b17fe981fc4c88dd1419d17eacc8d89ca8aa0
fivedee <64674962+fivedee@users.noreply.github.com>
2025-11-10 07:39:03 -0800

    Adjust RGB matrix colors to match line nav arrows.

--- users/fivedee/rgb.h
+++ users/fivedee/rgb.h
@@ -2,9 +2,9 @@
 #include QMK_KEYBOARD_H
 
 bool rgb_matrix_indicators_user(void) {
-    rgb_matrix_set_color(44, 0, 191, 111);
+    rgb_matrix_set_color(46, 0, 191, 111);
     rgb_matrix_set_color(38, 234, 58, 120);
     rgb_matrix_set_color(43, 72, 101, 214);
-    rgb_matrix_set_color(46, 244, 48, 62);
+    rgb_matrix_set_color(49, 244, 48, 62);
     return true;
 }
